d91a1692180b2e966b7ced508706369a60c8856a
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-07 13:40:14 -0500

    Add code to map next 1000 of json object - not running it yet

--- app/assets/javascripts/crimes.js
+++ app/assets/javascripts/crimes.js
@@ -1,6 +1,7 @@
 
 // API Call
 var sodaUrl = "https://data.seattle.gov/resource/3k2p-39jp.json?$where=within_box(incident_location, 47.615152, -122.351639, 47.575152, -122.311639)";
+var sodaUrl1000 = "https://data.seattle.gov/resource/3k2p-39jp.json?$where=within_box(incident_location, 47.615152, -122.351639, 47.575152, -122.311639)&$offset=1000";
 
 $.getJSON(sodaUrl, function(rawData) {
 	var styleArray = [
@@ -38,6 +39,43 @@ $.getJSON(sodaUrl, function(rawData) {
   }
 	google.maps.event.addDomListener(window, 'load');
 });
+
+// $.getJSON(sodaUrl1000, function(rawData) {
+// 	var styleArray = [
+// 	  {
+// 	    featureType: "all",
+// 	    stylers: [
+// 	      { saturation: -80 }
+// 	    ]
+// 	  },{
+// 	    featureType: "road.arterial",
+// 	    elementType: "geometry",
+// 	    stylers: [
+// 	      { hue: "#00ffee" },
+// 	      { saturation: 50 }
+// 	    ]
+// 	  },{
+// 	    featureType: "poi.business",
+// 	    elementType: "labels",
+// 	    stylers: [
+// 	      { visibility: "off" }
+// 	    ]
+// 	  }
+//   ];
+//   var mapOptions = {
+//     center: { lat: 47.595152, lng: -122.331639},
+//     zoom: 15,
+//     styles: styleArray,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//   };
+// 	var map = new google.maps.Map(document.getElementById('map-canvas'),
+//       mapOptions);
+// 	var data_to_map = prepareDataforMap(rawData);
+// 	for (i = 0; i < data_to_map.length; i++) {
+//     addMarker(data_to_map[i][0], data_to_map[i][1], data_to_map[i][2], data_to_map[i][3],data_to_map[i][4],data_to_map[i][5], data_to_map[i][6], data_to_map[i][7], map)
+//   }
+// 	google.maps.event.addDomListener(window, 'load');
+// });
 
 function prepareDataforMap(data) {
 	var dataArr = new Array();
