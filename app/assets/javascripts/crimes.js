2b6279d44d72d23de9e1d1f8758593dfe7b33171
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-07 15:09:31 -0500

    Add heatmap to map with toggle of information

--- app/assets/javascripts/crimes.js
+++ app/assets/javascripts/crimes.js
@@ -1,11 +1,17 @@
 
 // API Call
-var sodaUrlfirst1000 = "https://data.seattle.gov/resource/3k2p-39jp.json?$where=within_box(incident_location, 47.615152, -122.351639, 47.575152, -122.311639)";
+var sodaUrls = "https://data.seattle.gov/resource/3k2p-39jp.json?$where=within_box(incident_location, 47.615152, -122.351639, 47.575152, -122.3116390)&$limit=500";
 var sodaUrlsecond1000 = "https://data.seattle.gov/resource/3k2p-39jp.json?$where=within_box(incident_location, 47.615152, -122.351639, 47.575152, -122.311639)&$offset=1000";
+var sodaUrlthird1000 = "https://data.seattle.gov/resource/3k2p-39jp.json?$where=within_box(incident_location, 47.615152, -122.351639, 47.575152, -122.311639)&$offset=2000";
+
 
 google.maps.event.addDomListener(window, 'load', drawMap);
-mapCrimeData(sodaUrlfirst1000);
-mapCrimeData(sodaUrlsecond1000);
+// mapCrimeData(sodaUrlfirst1000);
+// mapCrimeData(sodaUrlsecond1000);
+// mapCrimeData(sodaUrlthird1000);
+addHeatMap(sodaUrlsecond1000);
+addHeatMap(sodaUrlthird1000)
+addHeatMap(sodaUrlfirst1000);
 
 function mapCrimeData(url) {
 	$.getJSON(url,function(data) {
@@ -15,6 +21,57 @@ function mapCrimeData(url) {
 	  }
 	});
 }
+
+var heatmap;
+function addHeatMap(url) {
+	$.getJSON(url,function(data) {
+		var dataArr = new Array();
+		var data_to_map = prepareDataforMap(data);
+		for (i = 0; i < data_to_map.length; i++) {
+	    dataArr.push(
+	    	new google.maps.LatLng(parseFloat(data_to_map[i][0]), parseFloat(data_to_map[i][1]))
+	    	)
+	  }
+	  console.log(dataArr)
+	  var pointArray = new google.maps.MVCArray(dataArr);
+		heatmap = new google.maps.visualization.HeatmapLayer({data:pointArray});
+		heatmap.setMap(map);
+	});
+}
+
+function toggleHeatmap() {
+  heatmap.setMap(heatmap.getMap() ? null : map);
+}
+
+function changeGradient() {
+  var gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 0, 0, 1)'
+  ]
+  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}
+
+function changeRadius() {
+  heatmap.set('radius', heatmap.get('radius') ? null : 20);
+}
+
+function changeOpacity() {
+  heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+}
+
+
 
 var styleArray = [
 {
@@ -39,7 +96,7 @@ var styleArray = [
 ];
 var mapOptions = {
   center: { lat: 47.595152, lng: -122.331639},
-  zoom: 15,
+  zoom: 13,
   styles: styleArray,
   mapTypeId: google.maps.MapTypeId.ROADMAP
 };
@@ -90,5 +147,4 @@ function createMarker(latitude, longitude, hundred_block_location, general_offen
       infowindow.open(map, marker);
   });
 }
-
 
